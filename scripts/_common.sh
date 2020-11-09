14eb4b985f102932ef104879aa075cccd55c7664
anmol <anmol@jswan2.com>
2020-11-09 20:16:29 +0530

    use pip3 to install dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python-msgpack python-gevent"
+pkg_dependencies="python3-msgpack python3-socks python3-gevent"
 
 #=================================================
 # PERSONAL HELPERS
