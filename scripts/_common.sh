eb212409f5baa62734f190889c30fdca8543481b
Amo <parrot@localhost.localdomain>
2021-05-23 22:15:38 +0530

    Fixed Zeronet mess which breaks Yunohost

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-venv python3-pip"
+pkg_dependencies="python3-venv python3-pip python3-gevent-websocket python3-msgpack python3-gevent python3-base58 python3-rsa python3-pyasn1 python3-websocket python3-maxminddb"
 
 #=================================================
 # PERSONAL HELPERS
